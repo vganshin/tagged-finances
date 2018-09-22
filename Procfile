358da52d1c177979e3cb66e1312a3d977de70029
Vladislav Ganshin <ganshinv@gmail.com>
2018-09-22 17:07:45 +0300

    Init Heroku app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/tagged-finances-standalone.jar clojure.main -m tagged-finances.web
---
+++ test/tagged_finances/web_test.clj
@@ -0,0 +1,6 @@
+(ns tagged-finances.web-test
+  (:require [clojure.test :refer :all]
+            [tagged-finances.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
