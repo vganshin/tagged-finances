199b6ebb312ab03ce6d25c7ad6db27dc704b370f
komantnick <sayapin6@mail.ru>
2018-10-16 16:46:47 +0300

    Api of transactions assembled v.2

--- src/tagged_finances/model.clj
+++ src/tagged_finances/model.clj
@@ -8,7 +8,7 @@
             [clj-time.format :as f]
             [clj-time.core :as t]))
 
-(def db {:connection-uri (or (env :jdbc-database-url) "jdbc:postgresql://localhost:5434/agile_backend?user=admin&password=pass")})
+(def db {:connection-uri (or (env :jdbc-database-url) "jdbc:postgresql://localhost:5432/agile_backend?user=postgres")})
 
     ; getting the current date
 (defn now [] (new java.util.Date))
@@ -24,7 +24,7 @@
 
 (defn create-deposit [params]
   (first (sql/insert! db :deposits {:name (let [name  (get params "name")] name)
-                             :balance (let [balance  (get params "balance")] balance)})))
+                                    :balance (let [balance  (get params "balance")] balance)})))
 
 (defn update-deposit [id params]
   (sql/update! db :deposits {:name (let [name  (get params "name")] name)
@@ -32,36 +32,30 @@
                ["id = ?" id]))
 
 (defn delete-deposit [id]
-  (sql/delete! db :deposits ["id = ?", id])) 
-    
-    
-    (defn create-transaction [params]
-       (first(sql/insert! db :transactions {:deposit_id (let [deposit_id  (get params "deposit_id")] deposit_id)
-                                        :amount (let [amount  (get params "amount")] amount)
-                                        :creation_ts (let [creation_ts  (get params "date")]  (c/to-sql-date creation_ts))
-                                        :tags (let [tags  (get params "tags")] tags)
-                                      }
-        )
-    ))
+  (sql/delete! db :deposits ["id = ?", id]))
 
-  (defn update-transaction [id params]
-    (sql/update! db :transactions 
-      {:deposit_id (let [deposit_id  (get params "deposit_id")] deposit_id)
+
+(defn create-transaction [params]
+  (first (sql/insert! db :transactions {:deposit_id (let [deposit_id  (get params "deposit_id")] deposit_id)
                                         :amount (let [amount  (get params "amount")] amount)
                                         :creation_ts (let [creation_ts  (get params "date")]  (c/to-sql-date creation_ts))
-                                        :tags (let [tags  (get params "tags")] tags)
-                                      }
-                                      ["id = ?" id]))
+                                        :tags (let [tags  (get params "tags")] tags)})))
+
+(defn update-transaction [id params]
+  (sql/update! db :transactions
+               {:deposit_id (let [deposit_id  (get params "deposit_id")] deposit_id)
+                :amount (let [amount  (get params "amount")] amount)
+                :creation_ts (let [creation_ts  (get params "date")]  (c/to-sql-date creation_ts))
+                :tags (let [tags  (get params "tags")] tags)}
+               ["id = ?" id]))
 (sql/update! db :transactions {:deposit_id 2} ["id=?" 14])
- (update-transaction 14  {
-  "date" "2018-10-12",
-  "tags" "tag1,tag2,tag3,tag4",
-  "deposit_id" 1,
-  "amount" 40,
-})
+(update-transaction 14  {"date" "2018-10-12"
+                         "tags" "tag1,tag2,tag3,tag4"
+                         "deposit_id" 1
+                         "amount" 40})
 
-    (defn select-transaction []
-      (sql/query db ["select * from transactions"]))
+(defn select-transaction []
+  (sql/query db ["select * from transactions"]))
 
-      (defn delete-transaction [id]
-        (sql/delete! db :transactions ["id = ?", id])) 
+(defn delete-transaction [id]
+  (sql/delete! db :transactions ["id = ?", id]))
