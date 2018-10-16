5ac2eca7d45676097f5adc383aa689485542bee2
Vladislav Ganshin <ganshinv@gmail.com>
2018-10-16 21:34:48 +0300

    delete on cascde

--- resources/migrations/20151206154000-create-tables.down.sql
+++ resources/migrations/20151206154000-create-tables.down.sql
@@ -1,4 +1,4 @@
-drop table if exists transactions;
-drop table if exists deposits;
-drop table if exists tags;
-drop table if exists tags_transactions;
+drop table if exists transactions cascade;
+drop table if exists deposits cascade;
+drop table if exists tags cascade;
+drop table if exists tags_transactions cascade;
